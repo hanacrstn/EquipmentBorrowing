25102b4d1444f45e7e7d2b30086ad9d99156d5d8
hc <hanakristinhc@gmail.com>
2026-08-28 00:30:37 +0800

    Equipment domain, repository, & in-memory

--- EquipmentBorrowing/src/EquipmentBorrowing.Application/Interfaces/IEquipmentRepository.cs
+++ EquipmentBorrowing/src/EquipmentBorrowing.Application/Interfaces/IEquipmentRepository.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using EquipmentBorrowing.Domain;
+﻿using EquipmentBorrowing.Domain;
 
 namespace EquipmentBorrowing.Application.Interfaces;
 
 public interface IEquipmentRepository
 {
     Task<Equipment?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
-
-    Task<IReadOnlyList<Equipment>> GetAvailableAsync(CancellationToken cancellationToken = default);
 
     Task UpdateAsync(Equipment equipment, CancellationToken cancellationToken = default);
 }
---
+++ EquipmentBorrowing/src/EquipmentBorrowing.Infrastructure/Repositories/InMemoryEquipmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using EquipmentBorrowing.Application.Interfaces;
+using EquipmentBorrowing.Domain;
+
+namespace EquipmentBorrowing.Infrastructure.Repositories;
+
+public class InMemoryEquipmentRepository : IEquipmentRepository
+{
+    private readonly List<Equipment> _equipment = new();
+
+    public void Seed(Equipment equipment) => _equipment.Add(equipment);
+
+    public Task<Equipment?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var item = _equipment.FirstOrDefault(e => e.Id == id);
+        return Task.FromResult(item);
+    }
+
+    public Task UpdateAsync(Equipment equipment, CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+}
