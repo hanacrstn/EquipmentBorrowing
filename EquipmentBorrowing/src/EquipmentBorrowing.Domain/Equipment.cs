07808dee845ce46d14a00d5eb31db01567bcfd09
hc <hanakristinhc@gmail.com>
2026-08-27 22:35:20 +0800

    Domain models

---
+++ EquipmentBorrowing/src/EquipmentBorrowing.Domain/Equipment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquipmentBorrowing.Domain;
+
+public class Equipment
+{
+    public int Id { get; }
+    public string Name { get; }
+    public bool IsAvailable { get; private set; }
+
+    public Equipment(int id, string name, bool isAvailable = true)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Equipment name is required.", nameof(name));
+
+        Id = id;
+        Name = name;
+        IsAvailable = isAvailable;
+    }
+
+    public void MarkAsBorrowed()
+    {
+        if (!IsAvailable)
+            throw new InvalidOperationException($"Equipment '{Name}' is already borrowed.");
+
+        IsAvailable = false;
+    }
+
+    public void MarkAsAvailable() => IsAvailable = true;
+}
---
+++ EquipmentBorrowing/src/EquipmentBorrowing.Domain/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquipmentBorrowing.Domain;
+
+public class Student
+{
+    public int Id { get; }
+    public string Name { get; }
+    public bool IsAllowedToBorrow { get; private set; }
+
+    public Student(int id, string name, bool isAllowedToBorrow = true)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Student name is required.", nameof(name));
+
+        Id = id;
+        Name = name;
+        IsAllowedToBorrow = isAllowedToBorrow;
+    }
+
+    public void Suspend() => IsAllowedToBorrow = false;
+
+    public void Reinstate() => IsAllowedToBorrow = true;
+}
